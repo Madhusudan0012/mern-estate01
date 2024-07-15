3d9f7c8a438ac0cafd7b5d27c87083ac348a2566
Madhusudan Singh Chauhan <136247818+Madhusudan0012@users.noreply.github.com>
2024-07-15 14:00:48 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect("mongodb+srv://Madhusudan:Madh62@#@mern-estate.n9r2fax.mongodb.net/?retryWrites=true&w=majority&appName=mern-estate")
-
-
+mongoose.connect(process.env.MONGO).then(()=> {
+    console.log('Connected to MongoDB!');
+}).catch((error)=>{
+    console.log(error);
+})
 
 
 const app = express();
 
 
-app.listen(3000, () => {
-    console.log('Server is running on pat 3000!');
+app.listen(3010, () => {
+    console.log('Server is running on port 3000!');
 }
 );
